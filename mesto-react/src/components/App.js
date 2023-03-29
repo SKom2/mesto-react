ea37952929279a324ffe0393ddcfad8a25c3ac90
Alex Spark <san4es.komgmail.com>
2023-03-29 21:32:43 +0300

    feat: Попапы открываются декларативным подходом

--- mesto-react/src/components/App.js
+++ mesto-react/src/components/App.js
@@ -3,51 +3,85 @@ import {Header} from "./Header";
 import {Main} from "./Main";
 import {Footer} from "./Footer";
 import {PopupWithForm} from "./PopupWithForm";
-import {ImagePopup} from "./ImagePopup";
+import React from "react";
+
 function App() {
-  return (
-    <>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+    const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false);
+    const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
+    const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
 
-      <template id="card-template">
-        <article className="card">
-            <img alt=""  className="card__image"></img>
-            <div className="card__heading">
-              <h2 className="card__title"></h2>
-              <div className="card__likes">
-                <button type="button" className="card__like"></button>
-                <span className="card__like-counter"></span>
-              </div>
-            </div>
-            <button type="button" className="card__delete"></button>
-        </article>
-      </template>
+    function handleEditProfileClick() {
+        if (isEditProfilePopupOpen === false) {
+            const popupEditProfile = document.querySelector('#popup_edit-profile');
+            popupEditProfile.classList.add('popup_opened');
+            setEditProfilePopupOpen(!isEditProfilePopupOpen);
+        }
+    }
 
-      <PopupWithForm name="edit-profile" title="Редактировать профиль" button="Сохранить">
-          <input type="text" className="form__input form__input_el_heading" id="name" name="name" placeholder="Имя" minLength="2" maxLength="40" required></input>
-          <span className="form__input-error name-error"></span>
-          <input type="text" className="form__input form__input_el_subheading" id="about-person" name="about" placeholder="О себе" minLength="2" maxLength="200" required></input>
-          <span className="form__input-error about-person-error"></span>
-      </PopupWithForm>
-      <PopupWithForm name="add-cards" title="Новое место" button="Создать">
-          <input type="text" className="form__input form__input_el_heading" id="photo-name" name="name" placeholder="Название" minLength="2" maxLength="30" required></input>
-          <span className="form__input-error photo-name-error"></span>
-          <input type="url" className="form__input form__input_el_subheading" id="photo-link" name="link" placeholder="Ссылка на картинку" required></input>
-          <span className="form__input-error photo-link-error"></span>
-      </PopupWithForm>
-      <PopupWithForm name="photo">
-          <img alt="#" className="popup__image"></img>
-          <h2 className="popup__title"></h2>
-      </PopupWithForm>
-      <ImagePopup></ImagePopup>
-      <PopupWithForm name="edit-avatar" title="Обновить аватар" button="Сохранить">
-          <input type="url" className="form__input form__input_el_heading" id="avatar-link" name="link" placeholder="Ссылка на картинку" required></input>
-          <span className="form__input-error avatar-link-error"></span>
-      </PopupWithForm>
-    </>
-  );
-      }
+    function handleAddPlaceClick() {
+        if (isAddPlacePopupOpen === false) {
+            const popupAddPlace = document.querySelector('#popup_add-cards');
+            popupAddPlace.classList.add('popup_opened');
+            setAddPlacePopupOpen(!isAddPlacePopupOpen)
+        }
+    }
+
+    function handleEditAvatarClick(){
+        if (isAddPlacePopupOpen === false) {
+            const popupEditAvatar = document.querySelector('#popup_edit-avatar')
+            popupEditAvatar.classList.add('popup_opened');
+            setEditAvatarPopupOpen(!isEditAvatarPopupOpen);
+        }
+    }
+
+    return (
+        <>
+          <Header></Header>
+          <Main
+              onEditProfile = {handleEditProfileClick}
+              onAddPlace = {handleAddPlaceClick}
+              onEditAvatar = {handleEditAvatarClick}
+          >
+          </Main>
+          <Footer></Footer>
+
+          <template id="card-template">
+            <article className="card">
+                <img alt=""  className="card__image"></img>
+                <div className="card__heading">
+                  <h2 className="card__title"></h2>
+                  <div className="card__likes">
+                    <button type="button" className="card__like"></button>
+                    <span className="card__like-counter"></span>
+                  </div>
+                </div>
+                <button type="button" className="card__delete"></button>
+            </article>
+          </template>
+
+          <PopupWithForm name="edit-profile" title="Редактировать профиль" button="Сохранить">
+              <input type="text" className="form__input form__input_el_heading" id="name" name="name" placeholder="Имя" minLength="2" maxLength="40" required></input>
+              <span className="form__input-error name-error"></span>
+              <input type="text" className="form__input form__input_el_subheading" id="about-person" name="about" placeholder="О себе" minLength="2" maxLength="200" required></input>
+              <span className="form__input-error about-person-error"></span>
+          </PopupWithForm>
+          <PopupWithForm name="add-cards" title="Новое место" button="Создать">
+              <input type="text" className="form__input form__input_el_heading" id="photo-name" name="name" placeholder="Название" minLength="2" maxLength="30" required></input>
+              <span className="form__input-error photo-name-error"></span>
+              <input type="url" className="form__input form__input_el_subheading" id="photo-link" name="link" placeholder="Ссылка на картинку" required></input>
+              <span className="form__input-error photo-link-error"></span>
+          </PopupWithForm>
+          <PopupWithForm name="photo">
+              <img alt="#" className="popup__image"></img>
+              <h2 className="popup__title"></h2>
+          </PopupWithForm>
+          {/*<ImagePopup></ImagePopup>*/}
+          <PopupWithForm name="edit-avatar" title="Обновить аватар" button="Сохранить">
+              <input type="url" className="form__input form__input_el_heading" id="avatar-link" name="link" placeholder="Ссылка на картинку" required></input>
+              <span className="form__input-error avatar-link-error"></span>
+          </PopupWithForm>
+        </>
+      );
+    }
 
 export default App;
--- mesto-react/src/components/Main.js
+++ mesto-react/src/components/Main.js
@@ -1,35 +1,19 @@
-export function Main() {
-
-    function handleEditAvatarClick(){
-        const popupEditAvatar = document.querySelector('#popup_edit-avatar')
-        popupEditAvatar.classList.add('popup_opened')
-    }
-
-    function handleEditProfileClick() {
-        const popupEditProfile = document.querySelector('#popup_edit-profile');
-        popupEditProfile.classList.add('popup_opened')
-    }
-
-    function handleAddPlaceClick() {
-        const popupAddPlace = document.querySelector('#popup_add-cards');
-        popupAddPlace.classList.add('popup_opened')
-    }
-
+export function Main(props) {
     return(
         <main className="main">
             <section className="main__profile profile root__container">
                 <div className="profile__body">
-                    <button className="profile__edit-avatar" onClick={handleEditAvatarClick}>
+                    <button className="profile__edit-avatar" onClick={props.onEditAvatar}>
                         <img src="mesto-react/src#" alt="Аватар" className="profile__avatar"></img>
                     </button>
                     <div className="profile__info">
                         <div className="profile__header">
                             <h1 className="profile__title">Жак Ив Кусто</h1>
-                            <button aria-label="Кнопка для открытия попапа" type="button" className="profile__button profile__button_type_edit button" id="button_edit" onClick={handleEditProfileClick}></button>
+                            <button aria-label="Кнопка для открытия попапа" type="button" className="profile__button profile__button_type_edit button" id="button_edit" onClick={props.onEditProfile}></button>
                         </div>
                         <p className="profile__subtitle">Исследователь океана</p>
                     </div>
-                    <button type="button" className="profile__button profile__button_type_add button" id="button_add" onClick={handleAddPlaceClick}></button>
+                    <button type="button" className="profile__button profile__button_type_add button" id="button_add" onClick={props.onAddPlace}></button>
                 </div>
             </section>
 
